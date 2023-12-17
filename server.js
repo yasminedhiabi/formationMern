9166c810450f994f96864860d0a87a1431c6aa49
yasmine <dhiabiyasmine@gmail.com>
2023-12-17 12:19:57 +0100

    connect to

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // import test from './test.js'
 // console.log("this is my server:")
 // test(2,6)
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
 
@@ -12,3 +13,5 @@ app.get("/verify", (req, res) => {
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+mongoose.connect('mongodb://127.0.0.1:1701/dantabase')
+  .then(() => console.log('Connected!'));
